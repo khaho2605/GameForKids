e76c744dab392a422ea170c0e651104d65c49ce6
Kha <khaho@executionlab.asia>
2018-12-10 20:02:53 +0700

    init project

---
+++ GameForKids/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GameForKids
+//
+//  Created by KHA on 12/10/18.
+//  Copyright © 2018 Kha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
